5f64c22bc92177bf85257ae2d2eda2529ebb7e7c
Kalane <vkaelin@hey.com>
2023-05-02 16:41:53 +0200

    style: format all files

--- src/main/scala/MainChatroom.scala
+++ src/main/scala/MainChatroom.scala
@@ -4,7 +4,9 @@ import Data._
 import Utils._
 
 object MainChatroom extends cask.Main:
-  val spellCheckerSvc: SpellCheckerService = new SpellCheckerImpl(Dictionary.dictionary)
+  val spellCheckerSvc: SpellCheckerService = new SpellCheckerImpl(
+    Dictionary.dictionary
+  )
   val tokenizerSvc: TokenizerService = new TokenizerService(spellCheckerSvc)
   val productSvc: ProductService = new ProductImpl()
   val sessionSvc: SessionService = new SessionImpl()
@@ -13,10 +15,9 @@ object MainChatroom extends cask.Main:
   val analyzerSvc: AnalyzerService = new AnalyzerService(productSvc, accountSvc)
 
   val allRoutes = Seq(
-      StaticRoutes(),
-      UsersRoutes(accountSvc, sessionSvc),
-      MessagesRoutes(tokenizerSvc, analyzerSvc, msgSvc, accountSvc, sessionSvc))
+    StaticRoutes(),
+    UsersRoutes(accountSvc, sessionSvc),
+    MessagesRoutes(tokenizerSvc, analyzerSvc, msgSvc, accountSvc, sessionSvc)
+  )
 
   override def port: Int = 8980
-
-
--- src/main/scala/Web/Layouts.scala
+++ src/main/scala/Web/Layouts.scala
@@ -3,9 +3,8 @@ package Web
 import scalatags.Text.all._
 import scalatags.Text.tags2
 
-/**
- * Assembles the method used to layout ScalaTags
- */
+/** Assembles the method used to layout ScalaTags
+  */
 object Layouts:
-    // You can use it to store your methods to generate ScalaTags.
+// You can use it to store your methods to generate ScalaTags.
 end Layouts
